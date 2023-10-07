06dde80baf6e57268ccbe188664f534ec9f8b8ff
eskoniarek <98158467+eskoniarek@users.noreply.github.com>
2023-10-07 21:43:18 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'KS',
+    siteUrl: 'https://ksfa.netlify.app/'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
